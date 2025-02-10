32957c0a5de5826ff79bdc4061f23634c9e12e20
piotr.kalota <piotr.kalota@feld-m.de>
2025-02-10 12:15:56 +0100

    Add new embedding model docs

--- docs/create_docs.py
+++ docs/create_docs.py
@@ -188,6 +188,9 @@ def update_mkdocs_yml(nav_structure):
         {
             "How to": [
                 {"How To Add a New LLM": "how_to/how_to_add_new_llm.md"},
+                {
+                    "How To Add a New Embedding Model": "how_to/how_to_add_new_embedding_model.md"
+                },
             ]
         },
         {
